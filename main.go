4e3056e616a54a8a9ee93399cdd2cf38b4dd6eb0
bornium-bn11 <bornium.bn11@gmail.com>
2023-12-22 18:47:59 -0300

    aa

--- main.go
+++ main.go
@@ -10,14 +10,16 @@ func main() {
 
 	fmt.Println("AAAA")
 
-	a01 := teste.Paulo_Cesar_A2222()
+	a01 := teste.Paulo_Cesar_A3333()
 
 	fmt.Println(a01)
 
 	b01 := teste.ConexaoRest_A2222()
 	fmt.Println(b01)
 
-	co1 := teste.Teste_A2222()
+	/*
+		co1 := teste.Teste_A2222()
 	fmt.Println(c01)
+	*/
 	
 }
