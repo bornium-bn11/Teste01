b1534bb6b75b4aeceaf7256d32c252f9cdc1ba67
bornium-bn11 <bornium.bn11@gmail.com>
2023-12-23 07:58:02 -0300

    Ultimo

--- main.go
+++ main.go
@@ -3,21 +3,21 @@ package main
 import (
 	"fmt"
 
-	teste "github.com/bornium-bn11/teste02"
+	teste "github.com/bornium-bn11/teste02/releases/tag/1.2"
 )
 
 func main() {
 
 	fmt.Println("AAAA")
 
-	a01 := teste.ConexaoRest_A44444()
+	a01 := teste.ConexaoRest_A555()
 
 	fmt.Println(a01)
 
-	b01 := teste.ConexaoRest_A44444()
+	b01 := teste.ConexaoRest_A555()
 	fmt.Println(b01)
 
-	c01 := teste.Teste_A44444()
+	c01 := teste.Teste_A55555()
 	fmt.Println(c01)
 
 }
